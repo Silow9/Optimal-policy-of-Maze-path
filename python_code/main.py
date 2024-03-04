79c1351f66909ca66f0c59ba5a065652e7cf8c37
shuhaoliu <liu.shuha@northeastern.edu>
2024-03-04 18:09:45 -0500

    3/4 code

--- python_code/main.py
+++ python_code/main.py
@@ -49,41 +49,40 @@ if __name__ == "__main__":
 
     # Policy_iteration
     # Base Scenario
-    # gridworld1 = GridWorld.create(State_Matrix, rewardlist)
-    # gridworld1.noise = 0.02/3
-    # gridworld1.discount_factor = 0.95
-    # policy = TabularPolicy(default_action=gridworld1.LEFT)
-    # iterations, values = PolicyIteration(gridworld1, policy).policy_iteration(max_iterations=100, theta=0.01)
-    # print("Number of iterations until convergence: %d" % (iterations))
-    # # gridworld1.visualise_policy(policy, title='Policy for Base Scenario')
-    # gridworld1.visualise_value_function(values, title="Optimal Value Function for Base Scenario", grid_size=1.5)
-    # # optimal_path = gridworld1.find_optimal_path(policy)
-    # # gridworld1.plot_optimal_path(optimal_path, "Optimal Path for Base Scenario")
+    gridworld1 = GridWorld.create(State_Matrix, rewardlist)
+    gridworld1.noise = 0.02/3
+    gridworld1.discount_factor = 0.95
+    policy = TabularPolicy(default_action=gridworld1.LEFT)
+    iterations, values = PolicyIteration(gridworld1, policy).policy_iteration(max_iterations=10000, theta=0.01)
+    print("Number of iterations until convergence: %d" % (iterations))
+    gridworld1.visualise_policy(policy, title='Policy for Base Scenario')
+    gridworld1.visualise_value_function(values, title="Optimal Value Function for Base Scenario", grid_size=1.5)
+    optimal_path = gridworld1.find_optimal_path(policy)
+    gridworld1.plot_optimal_path(optimal_path, "Optimal Path for Base Scenario")
 
-    #
-    # # Large Stochasticity Scenario
-    # gridworld2 = GridWorld.create(State_Matrix, rewardlist)
-    # gridworld2.noise = 0.5/3
-    # gridworld2.discount_factor = 0.95
-    # policy = TabularPolicy(default_action=gridworld2.LEFT)
-    # iterations, values = PolicyIteration(gridworld2, policy).policy_iteration(max_iterations=100, theta=0.01)
-    # print("Number of iterations until convergence: %d" % (iterations))
-    # gridworld2.visualise_policy(policy, title='Optimal Policy for Large Stochasticity Scenario')
-    # gridworld2.visualise_value_function(values, title="Optimal Value Function for Large Stochasticity Scenario", grid_size=1.5)
-    # optimal_path = gridworld2.find_optimal_path(policy)
-    # gridworld2.plot_optimal_path(optimal_path, "Large Stochasticity Scenario")
-    #
-    # # Small Discount Factor Scenario
-    # gridworld3 = GridWorld.create(State_Matrix, rewardlist)
-    # gridworld3.noise = 0.02/3
-    # gridworld3.discount_factor = 0.55
-    # policy = TabularPolicy(default_action=gridworld3.LEFT)
-    # iterations, values = PolicyIteration(gridworld3, policy).policy_iteration(max_iterations=100, theta=0.01)
-    # print("Number of iterations until convergence: %d" % (iterations))
-    # gridworld3.visualise_policy(policy, title='Optimal Policy for Small Discount Factor Scenario')
-    # gridworld3.visualise_value_function(values, title="Optimal Value Function for Small Discount Factor Scenario", grid_size=1.5)
-    # optimal_path = gridworld3.find_optimal_path(policy)
-    # gridworld3.plot_optimal_path(optimal_path, "Small Discount Factor Scenario")
+    # Large Stochasticity Scenario
+    gridworld2 = GridWorld.create(State_Matrix, rewardlist)
+    gridworld2.noise = 0.5/3
+    gridworld2.discount_factor = 0.95
+    policy = TabularPolicy(default_action=gridworld2.LEFT)
+    iterations, values = PolicyIteration(gridworld2, policy).policy_iteration(max_iterations=1000, theta=0.01)
+    print("Number of iterations until convergence: %d" % (iterations))
+    gridworld2.visualise_policy(policy, title='Optimal Policy for Large Stochasticity Scenario')
+    gridworld2.visualise_value_function(values, title="Optimal Value Function for Large Stochasticity Scenario", grid_size=1.5)
+    optimal_path = gridworld2.find_optimal_path(policy)
+    gridworld2.plot_optimal_path(optimal_path, "Large Stochasticity Scenario")
+
+    # Small Discount Factor Scenario
+    gridworld3 = GridWorld.create(State_Matrix, rewardlist)
+    gridworld3.noise = 0.02/3
+    gridworld3.discount_factor = 0.55
+    policy = TabularPolicy(default_action=gridworld3.LEFT)
+    iterations, values = PolicyIteration(gridworld3, policy).policy_iteration(max_iterations=100, theta=0.01)
+    print("Number of iterations until convergence: %d" % (iterations))
+    gridworld3.visualise_policy(policy, title='Optimal Policy for Small Discount Factor Scenario')
+    gridworld3.visualise_value_function(values, title="Optimal Value Function for Small Discount Factor Scenario", grid_size=1.5)
+    optimal_path = gridworld3.find_optimal_path(policy)
+    gridworld3.plot_optimal_path(optimal_path, "Small Discount Factor Scenario")
 
     # Value iteration
     # Base Scenario
@@ -91,27 +90,27 @@ if __name__ == "__main__":
     gridworldv1.noise = 0.02/3
     gridworldv1.discount_factor = 0.95
     values = TabularValueFunction()
-    ValueIteration(gridworldv1, values).value_iteration(max_iterations=4)
+    ValueIteration(gridworldv1, values).value_iteration(max_iterations=50)
     gridworldv1.visualise_value_function(values, "Value function for Base Scenario(Value iteration)")
     policy = values.extract_policy(gridworldv1)
     gridworldv1.visualise_policy(policy, "Policy for Base Scenario(Value iteration)")
 
-    # # Large Stochasticity Scenario
-    # gridworldv2 = GridWorld.create(State_Matrix, rewardlist)
-    # gridworldv2.noise = 0.02/3
-    # gridworldv2.discount_factor = 0.95
-    # values = TabularValueFunction()
-    # ValueIteration(gridworldv2, values).value_iteration(max_iterations=100)
-    # gridworldv2.visualise_value_function(values, "Value function for Large Stochasticity Scenario(Value iteration)")
-    # policy = values.extract_policy(gridworldv2)
-    # gridworldv2.visualise_policy(policy, "Policy for Large Stochasticity Scenario(Value iteration)")
-    #
-    # # Small Discount Factor Scenario
-    # gridworldv3 = GridWorld.create(State_Matrix, rewardlist)
-    # gridworldv3.noise = 0.02/3
-    # gridworldv3.discount_factor = 0.95
-    # values = TabularValueFunction()
-    # ValueIteration(gridworldv3, values).value_iteration(max_iterations=100)
-    # gridworldv3.visualise_value_function(values, "Value function for Small Discount Factor Scenario(Value iteration)")
-    # policy = values.extract_policy(gridworldv3)
-    # gridworldv3.visualise_policy(policy, "Policy for Small Discount Factor Scenario(Value iteration)")
+    # Large Stochasticity Scenario
+    gridworldv2 = GridWorld.create(State_Matrix, rewardlist)
+    gridworldv2.noise = 0.02/3
+    gridworldv2.discount_factor = 0.95
+    values = TabularValueFunction()
+    ValueIteration(gridworldv2, values).value_iteration(max_iterations=100)
+    gridworldv2.visualise_value_function(values, "Value function for Large Stochasticity Scenario(Value iteration)")
+    policy = values.extract_policy(gridworldv2)
+    gridworldv2.visualise_policy(policy, "Policy for Large Stochasticity Scenario(Value iteration)")
+
+    # Small Discount Factor Scenario
+    gridworldv3 = GridWorld.create(State_Matrix, rewardlist)
+    gridworldv3.noise = 0.02/3
+    gridworldv3.discount_factor = 0.95
+    values = TabularValueFunction()
+    ValueIteration(gridworldv3, values).value_iteration(max_iterations=100)
+    gridworldv3.visualise_value_function(values, "Value function for Small Discount Factor Scenario(Value iteration)")
+    policy = values.extract_policy(gridworldv3)
+    gridworldv3.visualise_policy(policy, "Policy for Small Discount Factor Scenario(Value iteration)")
